7adc455fc6c8674ca8ffcb9fff85c0e0498f292a
Eimyatnoeaung <annabel00789@gmail.com>
2020-11-24 16:23:12 +0630

    insert pizza datas to database

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pizza;
 use Illuminate\Http\Request;
 
 class PizzaController extends Controller
@@ -25,6 +26,15 @@ class PizzaController extends Controller
         ]);
 
         if($validation){
+            //insert data to database
+           $pizza=new Pizza();
+           $pizza->username=$req->username;
+           $pizza->pizza_name=$req->pizza_name;
+           $pizza->topping=$req->topping;
+           $pizza->sauce=$req->sauce;
+           $pizza->price=$req->price;
+           $pizza->save();
+        //    return $pizza;
             return back()->with("success","Thank You for your order");
         }else{
             return back()->withErrors($validation);
